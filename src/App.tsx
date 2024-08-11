4086a52b607df374280490d7f2de39bb04771d4c
Mahad Kalam <55807755+SkyfallWasTaken@users.noreply.github.com>
2024-08-11 01:53:58 +0000

    Broken typewriter effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,37 @@
 import { Suspense, useState, useEffect } from "react"
 
 export default function App() {
-  const [word, setWord] = useState<string | null>(null)
+  const [typed, setTyped] = useState("")
+  
   useEffect(() => {
     const fetchWord = async () => {
       const url = "https://wgg522pwivhvi5gqsn675gth3q0otdja.lambda-url.us-east-1.on.aws/636861"
       const response = await fetch(url)
-      setWord(await response.text())
+      const word = await response.text()
     }
 
     fetchWord()
   }, [])
+
+  useEffect(() => {
+    let i = 0;
+    const typingInterval = setInterval(() => {
+      if (i < word.length) {
+        setTyped(prevText => prevText + word.charAt(i));
+        i++;
+      } else {
+        clearInterval(typingInterval);
+      }
+    }, 500);
+
+    return () => {
+      clearInterval(typingInterval);
+    };
+  }, [word]);
   
   return (
     <main>
-      <p>{word}</p>
+      <p>{typed}</p>
     </main>
   )
 }
